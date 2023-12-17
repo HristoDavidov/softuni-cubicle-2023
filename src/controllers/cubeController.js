c769e809b6ca65e2c97c93afedfea32125e9ad54
HristoDavidov <hristo_davidov@mail.bg>
2023-12-17 15:18:07 +0100

    Add cube controller

---
+++ src/controllers/cubeController.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+router.get('/create', (req, res) => {
+    res.render('create');
+});
+
+router.post('/create', (req, res) => {
+    res.redirect('/');
+});
+
+
+module.exports = router;
