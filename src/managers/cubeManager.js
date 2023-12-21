30ce94c23d2bca6c5b24457f58bf46eadcd11377
HristoDavidov <hristo_davidov@mail.bg>
2023-12-21 19:40:38 +0100

    Add cube details page

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,7 +1,23 @@
 const uniqid = require('uniqid');
-const cubes = [];
+const cubes = [
+    {
+        id: '7y04a50lqf43cnr',
+        name: 'Mirror Cube',
+        description: 'A cool mirror cube',
+        imageUrl: 'https://img.fruugo.com/product/3/60/696637603_max.jpg',
+        difficultyLevel: 4,
+    },
+    {
+        id: '7y94a70lsf43cnr',
+        name: 'Rubic cube',
+        description: 'A clasic cube',
+        imageUrl:'https://m.media-amazon.com/images/I/81a7IEinLiL.jpg',
+        difficultyLevel: 2,
+    }
+];
 
 exports.getAll = () => cubes.slice(); 
+exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId);
 
 exports.create = (cubeData) => {
     const newCube = {
