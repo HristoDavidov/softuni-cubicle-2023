3aaf9e670e1bf6d37fd9e28e889e8f4bd6add6e4
HristoDavidov <hristo_davidov@mail.bg>
2024-01-30 10:42:46 +0100

    Add attach accessory link

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -35,8 +35,8 @@ router.get('/:cubeId/details', async (req, res) => {
     res.render('details', { cube });
 });
 
-router.get('/cubeId/attach-accessory', (req, res) => {
-    res.render('accessory/attach')
+router.get('/:cubeId/attach-accessory', (req, res) => {
+    res.render('accessory/attach');
 });
 
 
