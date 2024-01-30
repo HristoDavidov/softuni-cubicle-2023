b7b027bcae71c807dc8a3a644b0773ad90417c1d
HristoDavidov <hristo_davidov@mail.bg>
2024-01-30 15:42:03 +0100

    Show attached accessories

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -27,7 +27,7 @@ router.post('/create', async (req, res) => {
 
 router.get('/:cubeId/details', async (req, res) => {
 
-    const cube = await cubeManager.getOne(req.params.cubeId).lean();
+    const cube = await cubeManager.getOneWhitAccessories(req.params.cubeId).lean();
    
 
     if (!cube) {
